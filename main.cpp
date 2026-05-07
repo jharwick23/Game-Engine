e0a0f9b520e342e72e0afa933190ab40c6a82961
jharwick23 <jharwick12@gmail.com>
2026-05-07 18:07:41 -0400

    First triangle rendered :D

--- main.cpp
+++ main.cpp
@@ -3,6 +3,152 @@
 #include <glad/glad.h>
 #include <SDL2/SDL.h>
 
+float vertices[] = {
+    -0.5f, -0.5f,
+     0.5f, -0.5f,
+     0.0f,  0.5f
+};
+
+const char* vertexShaderSource = R"(
+#version 330 core
+
+layout (location = 0) in vec2 aPos;
+
+void main()
+{
+    gl_Position = vec4(aPos, 0.0, 1.0);
+}
+)";
+
+const char* fragmentShaderSource = R"(
+#version 330 core
+
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}
+)";
+
+unsigned int VAO, VBO;
+unsigned int shaderProgram;
+
+// Process input for the window
+static void processInput(bool &isRunning)
+{
+    // Process Input
+    SDL_Event event;
+
+    while(SDL_PollEvent(&event))
+    {
+        if(event.type == SDL_QUIT)
+        {
+            isRunning = false;
+        }
+
+        if(event.type == SDL_KEYDOWN)
+        {
+            if(event.key.keysym.sym == SDLK_ESCAPE)
+            {
+                isRunning = false;
+            }
+        }
+    }
+}
+
+// Render with OpenGL within the window
+static void render(SDL_Window *window)
+{
+    // Render
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shaderProgram);
+
+    glBindVertexArray(VAO);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    SDL_GL_SwapWindow(window);
+}
+
+// Generate the GPU Objects
+static void genGPUObj()
+{
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+}
+
+// Bind Objects
+static void bindGPUObj()
+{
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER,
+                sizeof(vertices),
+                vertices,
+                GL_STATIC_DRAW);
+}
+
+// Describe the vertex layout
+static void descVerLay()
+{
+    glVertexAttribPointer(
+        0,
+        2,
+        GL_FLOAT,
+        GL_FALSE,
+        2 * sizeof(float),
+        (void*)0
+    );
+
+    glEnableVertexAttribArray(0);
+}
+
+// Create the shaderprogram object
+static void createShaderProgram()
+{
+    // Vertex Shader
+    unsigned int vertexShader =
+        glCreateShader(GL_VERTEX_SHADER);
+
+    glShaderSource(
+        vertexShader,
+        1,
+        &vertexShaderSource,
+        nullptr
+    );
+
+    glCompileShader(vertexShader);
+
+    // Fragment Shader
+    unsigned int fragmentShader =
+        glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(
+        fragmentShader,
+        1,
+        &fragmentShaderSource,
+        nullptr
+    );
+
+    glCompileShader(fragmentShader);
+
+    // Shader Program
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+
+    glLinkProgram(shaderProgram);
+
+    // Cleanup shaders
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+}
+
 int main()
 {
     // Initializes a SDL video subsystem
@@ -56,6 +202,11 @@ int main()
         return -1;
     }
 
+    createShaderProgram();
+    genGPUObj();
+    bindGPUObj();
+    descVerLay();
+
     // Print OpenGL version
     std::cout << "OpenGL Version: "
               << glGetString(GL_VERSION)
@@ -66,30 +217,9 @@ int main()
 
     while(running)
     {
-        // Process Input
-        SDL_Event event;
+        processInput(running);
 
-        while(SDL_PollEvent(&event))
-        {
-            if(event.type == SDL_QUIT)
-            {
-                running = false;
-            }
-
-            if(event.type == SDL_KEYDOWN)
-            {
-                if(event.key.keysym.sym == SDLK_ESCAPE)
-                {
-                    running = false;
-                }
-            }
-        }
-
-        // Render
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        SDL_GL_SwapWindow(window);
+        render(window);
     }
 
     // Cleanup
